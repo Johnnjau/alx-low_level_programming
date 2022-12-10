9a7cbc4523b3079e2303e4e6de0ca120b91bbc47
Johnnjau <Johnnjau202@gmail.com>
2022-12-09 18:11:30 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar ('\n');
+	return (0);
+}
