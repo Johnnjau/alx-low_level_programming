8422278d8b105286159b7a6d43cf711b1d4df5df
Johnnjau <Johnnjau202@gmail.com>
2022-12-14 11:46:24 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,17 @@
-#include "main.h"
-#include <unistd.h>
+#include <stdio.h>
 /**
+ * main - print in lower and upper case
  *
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z' ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z' ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
