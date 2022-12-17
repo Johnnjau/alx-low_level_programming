2c1aa04b0f99fec047b3be6b42a42c0e4ca2f2a7
Johnnjau <Johnnjau202@gmail.com>
2022-12-16 17:21:26 -0800

    Task 12

---
+++ 0x03-debugging/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - prints and integer.
+ * @n input integer
+ * Return: no return.
+ */
+
+void print_number(int n);
+{
+	unsigned int m, d, count;
+	if (n < 0
+			)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+	d = m;
+	count = 1;
+}
