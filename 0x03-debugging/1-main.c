4d9d2bb4c933f08f14ed2015bb6e339ef7d0957a
Johnnjau <Johnnjau202@gmail.com>
2022-12-16 16:02:55 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,14 +12,14 @@ printf("infinite loop incoming :(\n");
 
 i = 0;
 
-/**
+/*
 *while (i < 10)
 *{
 *putchar(i);
 *}
 */
 
-printf("Infinite loop avoided !\\o / \n");
+printf("Infinite loop avoided !\\o /\n");
 
 return (0);
 }
